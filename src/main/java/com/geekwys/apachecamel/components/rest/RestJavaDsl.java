94329f2fdc1e7eec58baae71a2c3e3341938c13f
ShemBrooklyne <shemnyatti@gmail.com>
2022-06-10 14:23:13 +0300

    build message object

--- src/main/java/com/geekwys/apachecamel/components/rest/RestJavaDsl.java
+++ src/main/java/com/geekwys/apachecamel/components/rest/RestJavaDsl.java
@@ -1,15 +1,32 @@
 package com.geekwys.apachecamel.components.rest;
 
+import com.geekwys.apachecamel.dto.WeatherDto;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.DefaultMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestJavaDsl extends RouteBuilder {
 
+    @Autowired
+    private WeatherDataProvider weatherDataProvider;
     @Override
     public void configure() throws Exception {
         from("javadsl/weather/{city}?produces=application/json")
-                .process();
+                .process(this::getWeatherData);
+    }
+
+    private void getWeatherData(Exchange exchange) {
+        String city = exchange.getMessage().getHeader("city", String.class);
+        WeatherDto currentWeather = weatherDataProvider.getCurrentWeather(city);
+
+        Message message = new DefaultMessage(exchange.getContext());
+        message.setBody(currentWeather);
+
+        exchange.setMessage(message);
     }
 
 }
